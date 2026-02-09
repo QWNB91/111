d9db56e17dca93db39f588bd351f20d2f65f77ff
QWNB <3335753204@qq.com>
2026-02-09 22:51:45 +0800

    smnb
    shsj

---
+++ 砍伐树木.lua
@@ -0,0 +1,98 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "苏脚本 ",
+    Icon = "rbxassetid://70930606389113",
+    IconThemed = true,
+    Author = "by.SU",
+    Folder = "CloudHub",
+    Size = UDim2.fromOffset(300, 270),
+    Transparent = true,
+    Theme = "Dark",
+    SideBarWidth = 200,
+    HideSearchBar = true,
+    ScrollBarEnabled = true,
+    Background = "rbxassetid://96797361260081"
+})
+
+Window:Tag({
+    Title = "服务器：砍伐树木",
+    Color = WindUI:Gradient({
+        ["0"]   = { Color = Color3.fromHex("#FF0F7B"),  Transparency = 0.7 },
+        ["100"] = { Color = Color3.fromHex("#F89B29"), Transparency = 0.5 },
+    }, {
+        Rotation = 100,
+    }),
+    Radius = 10,
+})
+
+Window:Tag({
+    Title = (identifyexecutor and identifyexecutor() or getexecutorname and getexecutorname() or "未知"),
+    Color = WindUI:Gradient({
+        ["0"]   = { Color = Color3.fromHex("#FF0F7B"),  Transparency = 0.7 },
+        ["100"] = { Color = Color3.fromHex("#F89B29"), Transparency = 0.5 },
+    }, {
+        Rotation = 100,
+    }),
+    Radius = 10,
+})
+
+Window:Tag({
+    Title = "Beta",
+    Color = WindUI:Gradient({
+        ["0"]   = { Color = Color3.fromHex("#FF0F7B"),  Transparency = 0.7 },
+        ["100"] = { Color = Color3.fromHex("#F89B29"), Transparency = 0.5 },
+    }, {
+        Rotation = 100,
+    }),
+    Radius = 10,
+})
+
+
+Window:SetToggleKey(Enum.KeyCode.F)
+
+Window:EditOpenButton({
+    Title = "SUscript",
+    Icon = "rbxassetid://91866950234209",
+    CornerRadius = UDim.new(0, 16),
+    StrokeThickness = 2,
+    Color = ColorSequence.new(Color3.fromHex("FF0F7B"), Color3.fromHex("F89B29")),
+    OnlyMobile = false,
+    Draggable = true
+})
+
+TabSection = Window:Section({Title = "主页", Opened = true})
+
+local Tab = TabSection:Tab({Title = "Main", Icon = "check"})
+
+local sbxp = false
+Tab:Toggle({
+    Title = "范围砍树(可能会卡)",
+    Default = false,
+    Image = "check",
+    Callback = function(state)
+        sbxp = state
+        while sbxp and wait() do
+            for o,s in next, workspace.TreesFolder:GetChildren() do
+                game:GetService("ReplicatedStorage"):WaitForChild("Signal"):WaitForChild("Tree"):FireServer("damage",s.Name)
+            end
+        end
+    end
+})
+
+local sbleng = false
+Tab:Toggle({
+    Title = "范围捡宝箱(延迟太大就不能用)",
+    Default = false,
+    Image = "check",
+    Callback = function(state)
+        sbleng = state
+        while sbleng and wait() do
+            for o,s in next, workspace.ChestFolder:GetChildren() do
+                if s:FindFirstChild("Hitpart")then 
+                    fireproximityprompt(s.Hitpart.ProximityPrompt)
+                end
+            end
+        end
+    end
+})
